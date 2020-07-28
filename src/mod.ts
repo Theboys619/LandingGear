8eed1ee049be175dfb40cba3aacadbcd3b1792ff
theboys619 <theboys619@gmail.com>
2020-07-28 01:41:31 -0400

    added Worker Requests

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-import { contentType, Path, readFileRecursive } from "./helpers.ts";
+import { contentType, Path, readFileRecursive, exists, isHTML, convertExt, getRealpath } from "./helpers.ts";
 import { serve, Server, ServerRequest } from "https://deno.land/std/http/server.ts";
 
 import { HTTPRequest, Request, IListener } from "./request.ts";
@@ -7,25 +7,50 @@ import { RequestCallback, LGError, LGMiddleware, LGMiddlewareErr, isInstanceOfMi
 const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
 
-export interface LGOptions {}
+export interface LGOptions {
+  isWorker?: boolean
+  listeners?: Array<IListener>
+}
 export type MethodTypes = "GET" | "POST" | "OPTIONS";
 
 export class App {
   #options: LGOptions;
   #listeners: Array<IListener>;
+  #workers: Map<string, Worker>;
   #server: Server | undefined;
 
   constructor (options?: LGOptions) {
-    this.#options = options ?? {};
+    this.#options = options ?? { isWorker: false };
 
     this.#listeners = [];
+    this.#workers = new Map;
   }
 
   #monitorRequests = async () => {
     if (!this.#server) return;
     for await (const servreq of this.#server) {
       let httpreq = new HTTPRequest(this.#server, servreq);
-      httpreq.handleRequest(this.#listeners);
+      let data = `${httpreq.method}|${httpreq.getRequest().pathname}`;
+
+      if (this.#workers.has(data)) {
+        const worker = this.#workers.get(data) as Readonly<Worker>;
+
+        worker.addEventListener("message", (({ data }: MessageEvent) => {
+          servreq.respond({
+            status: data.status ?? 404,
+            headers: new Headers(data?.headers),
+            body: data?.body
+          });
+        }) as EventListener, {
+          once: true,
+          passive: true,
+          capture: true
+        });
+
+        worker.postMessage(undefined);
+      } else {
+        httpreq.handleRequest(this.#listeners);
+      }
     }
   }
 
@@ -72,6 +97,15 @@ export class App {
       });
     }
   }
+
+  createWorker(method: string, path: string, workerPath: string) {
+    this.#workers.set(`${method}|${path}`, new Worker(
+      new URL(workerPath, import.meta.url).href, {
+        type: "module",
+        deno: true
+      }
+    ));
+  }
   
   get(path: string, cb: RequestCallback): App;
   get(path: string, middleware: LGMiddlewareErr, cb: RequestCallback): App;
@@ -100,9 +134,18 @@ export class App {
 
 export class Router {
   #listeners: Array<IListener>;
+  #options: LGOptions;
+  pathname: string;
 
   constructor (options?: LGOptions) {
-    this.#listeners = [];
+    this.#listeners = options?.listeners ?? [];
+
+    this.#options = options ?? { isWorker: false };
+    this.pathname = Path.dirname(Path.fromFileUrl(new URL('.', import.meta.url).href));
+  }
+
+  getOptions() {
+    return this.#options;
   }
 
   #createListener = (method: MethodTypes, path: string, middleware: LGMiddleware | LGMiddlewareErr | RequestCallback, cb?: RequestCallback): void => {
@@ -153,13 +196,122 @@ export class Router {
   }
 }
 
+export class WorkerResponse {
+  worker: any;
+  status: number;
+  headers: Headers;
+  callbacks: Array<Function>;
+
+  pathname: string;
+  #fakepath: string;
+  
+  params: { [x: string]: any };
+
+  constructor (worker: any) {
+    this.worker = worker;
+
+    this.status = 404;
+    this.headers = new Headers();
+
+    this.callbacks = [];
+    this.params = {};
+    this.pathname = "";
+    this.#fakepath = "";
+
+    worker.addEventListener(
+      "message",
+      (e: MessageEvent) => {
+        for (let callback of this.callbacks) {
+          callback();
+        }
+      }
+    );
+  }
+
+  addCallback(cb: Function) {
+    this.callbacks.push(cb);
+  }
+
+  constructParams(): { [x: string]: any } {
+    if (this.#fakepath.startsWith("/")) this.#fakepath = this.#fakepath.substr(1);
+    if (this.#fakepath.endsWith("/")) this.#fakepath = this.#fakepath.slice(0, -1)
+
+    let paths = this.#fakepath.split("/");
+    let fullpaths = this.pathname.split("/");
+    let index = 0;
+
+    fullpaths.splice(0, 1);
+
+    for (let pathname of paths) {
+      if (pathname.startsWith(":")) {
+        pathname = pathname.substr(1);
+
+        this.params[pathname] = fullpaths[index];
+      }
+      index++;
+    }
+
+    return this.params;
+  }
+
+  setContentType(header: string, type: any) {
+    let typestring = contentType(type);
+    if (typestring && !this.headers.has(header)) {
+      this.headers.append(header, typestring);
+    }
+  }
+  
+  setHeader(header: string, value: any) {
+    this.headers.set(header, value);
+  }
+
+  sendFile(path: string, options?: { root: string }) {
+    if (options && options.root) path = Path.fromFileUrl(new URL(Path.join(options.root, path), import.meta.url).href);
+    else path = Path.fromFileUrl(new URL(path, import.meta.url).href);
+    let ext = path.split(".")[path.split(".").length-1] ?? "txt";
+    let datatype = convertExt(ext);
+    
+    Deno.readFile(path).then(data => {
+      let text = decoder.decode(data);
+      
+      if (typeof datatype == "string")
+        this.send(text, 200, datatype);
+      else
+        this.send(text, 200);
+    });
+  }
+  
+  send(data: any, status?: number, type?: string): boolean {
+    if (!status) this.status = 200;
+    else this.status = status;
+    if (typeof data == "object" || type == "object") {
+      data = JSON.stringify(data);
+      if (!type)
+        type = "application/json";
+
+      this.setHeader("Content-Length", data.toString().length);
+    } else if (["string", "number", "bigint", "boolean", "object"].includes(typeof data)) {
+      if (!type)
+        type = (isHTML(data)) ? "html" : "text/plain";
+    }
+
+    this.setContentType("Content-Type", type);
+
+    if (this.worker?.postMessage)
+      this.worker.postMessage({ body: data, headers: Array.from(this.headers.entries()), status: this.status });
+    return true;
+  }
+}
+
 export default function landingGear(options?: LGOptions): App {
   landingGear.app = new App(options);
+  landingGear.appCreated = true;
 
   return landingGear.app;
 }
 
 landingGear.app = new App();
+landingGear.appCreated = false;
 
 landingGear.Router = (options?: LGOptions) => {
   return new Router(options);
